89b0451e99a62df5f338dc75deff93d2ab8db702
michiya-59 <nishino.michiya0509@gmail.com>
2023-01-24 08:24:30 +0900

    メインビジュアル部分完成

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// ハンバーガーメニュー
 window.onload = function(){
   const hunburger_menu = document.getElementById('hunburger_menu');
   const header = document.getElementById('header');
@@ -7,4 +8,25 @@ window.onload = function(){
   hunburger_menu.addEventListener('click', () => {
     header.classList.toggle('open');
   });
+}
+
+// 写真のスライドショー
+window.addEventListener('load', function () {
+	viewSlide('.main_visual_right img');
+});
+
+function viewSlide(className, slideNo = -1){
+	let imgArray = document.querySelectorAll(className);
+	if (slideNo >= 0) {
+		//初回以外は現在のスライドを消す
+		imgArray[slideNo].style.opacity = 0;
+	}
+	slideNo++;
+	if (slideNo >= imgArray.length) {
+		slideNo = 0; //次のスライドがなければ最初のスライドへ戻る
+	}
+  console.log(imgArray);
+  console.log(slideNo);
+	imgArray[slideNo].style.opacity = 1;
+	setTimeout(function(){viewSlide(className, slideNo);}, 5000);
 }
