d7e907e1326a98b87a29e4a9a6422388b0f5b936
michiya-59 <nishino.michiya0509@gmail.com>
2023-01-25 09:19:15 +0900

    reason部分の完成

--- js/index.js
+++ js/index.js
@@ -25,8 +25,28 @@ function viewSlide(className, slideNo = -1){
 	if (slideNo >= imgArray.length) {
 		slideNo = 0; //次のスライドがなければ最初のスライドへ戻る
 	}
-  console.log(imgArray);
-  console.log(slideNo);
+
 	imgArray[slideNo].style.opacity = 1;
 	setTimeout(function(){viewSlide(className, slideNo);}, 5000);
+}
+
+// BBBが選ばれる理由 フェードイン
+window.onload = function(){
+  const reason_content_left = document.getElementById('reason_content_left');
+  const reason_content_right = document.getElementById('reason_content_right');
+
+  window.addEventListener('scroll', () => {
+    //フェードインする要素の上部の位置を取得する
+    const fedein_elem_top_left = reason_content_left.getBoundingClientRect().top;
+    const fedein_elem_top_right = reason_content_right.getBoundingClientRect().top;
+    //ブラウザの高さを取得
+    const window_height = window.innerHeight;
+    // 画面に表示された時にフェードインさせる処理
+    if (window_height > fedein_elem_top_left + 50){
+      reason_content_left.classList.add('fadein-after');
+    }
+    if (window_height > fedein_elem_top_right + 50){
+      reason_content_right.classList.add('fadein-after');
+    }
+  });
 }
