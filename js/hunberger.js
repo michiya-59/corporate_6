80c37ea6115d50c2c28a6c53e20f7529e96a1f9d
michiya-59 <nishino.michiya0509@gmail.com>
2023-01-27 00:58:24 +0900

    レスポンシブ対応完了

--- js/hunberger.js
+++ js/hunberger.js
@@ -5,9 +5,6 @@ window.addEventListener('load', () => {
   const hunburger_menu = document.getElementById('hunburger_menu');
   const header = document.getElementById('header');
   const mask_menu = document.getElementById('mask_menu');
-  const studio = document.getElementById('studio');
-  const summary = document.getElementById('summary');
-  const free_entry = document.getElementById('free_entry');
 
   hunburger_menu.addEventListener('click', () => {
     header.classList.toggle('open');
