473e11524a1a8cdf4f75fb94aa03c28a74a2f615
michiya-59 <nishino.michiya0509@gmail.com>
2023-01-26 14:43:03 +0900

    各リンクの修正

--- js/hunberger.js
+++ js/hunberger.js
@@ -4,10 +4,15 @@ window.addEventListener('load', () => {
   // ハンバーガーメニュー
   const hunburger_menu = document.getElementById('hunburger_menu');
   const header = document.getElementById('header');
-  console.log(hunburger_menu);
-  console.log(header);
+  const mask_menu = document.getElementById('mask_menu');
+  const studio = document.getElementById('studio');
+  const summary = document.getElementById('summary');
+  const free_entry = document.getElementById('free_entry');
 
   hunburger_menu.addEventListener('click', () => {
+    header.classList.toggle('open');
+  });
+  mask_menu.addEventListener('click', () => {
     header.classList.toggle('open');
   });
 });
